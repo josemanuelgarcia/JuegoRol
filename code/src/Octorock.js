1ef35ba7f7428eee3ed8581426ef3cd2e7300735
josemanuelgarcia <josececos@gmail.com>
2015-12-25 20:08:45 +0100

    MOdificado Space y pinto enemigo
    Modifique el space para que este dentro de la layer de juego y añadi
    algo mas a la clase enemigo  y disparo enemigo. De momento es la misma
    imagen qu elink pero es provisional

--- code/src/Octorock.js
+++ code/src/Octorock.js
@@ -11,6 +11,12 @@ var Octorock = cc.Class.extend({
 	sprite: null,
 	body: null,
 	shape: null,
+	//Sentido hacia el que esta mirando y a donde va a disparar
+	//1-> Arriba
+	//2-> Derecha
+	//3-> Abajo
+	//4-> Izquierda
+	sentido: 3,
 
 	ctor: function (space, posicion, layer) {
 		this.space = space;
@@ -19,7 +25,7 @@ var Octorock = cc.Class.extend({
 		//Primera prueba: que dispare cada cierto tiempo
 		this.tiempoEntreDisparos=2+Math.floor(Math.random()*2);
 
-		this.sprite = new cc.PhysicsSprite("#zelda_abajo0.png");//TODO cambiar animacion
+		this.sprite = new cc.PhysicsSprite("#link_abajo0.png");//TODO cambiar animacion
         // Cuerpo dinamico, SI le afectan las fuerzas
         this.body = new cp.Body(5, cp.momentForBox(1,
             this.sprite.getContentSize().width,
@@ -42,12 +48,12 @@ var Octorock = cc.Class.extend({
 
 
 		//Animacion Mover Abajo
-		var framesCaminarAbajo = this.getAnimacion("zelda_abajo", 6);
+		var framesCaminarAbajo = this.getAnimacion("link_abajo", 6);
 		var animacionAbajo = new cc.Animation(framesCaminarAbajo, 0.1);
 		this.animCaminarAbajo = cc.RepeatForever.create(new cc.Animate(animacionAbajo));
 
 		//Animacion Mover Arriba
-		var framesCaminarArriba = this.getAnimacion("zelda_arriba", 6);
+		var framesCaminarArriba = this.getAnimacion("link_arriba", 6);
 		var animacionArriba = new cc.Animation(framesCaminarArriba, 0.1);
 		this.animCaminarArriba = cc.RepeatForever.create(new cc.Animate(animacionArriba));
 
@@ -56,25 +62,22 @@ var Octorock = cc.Class.extend({
 		return true;
 
 
-	}, update: function (dt, jugadorX) {
+	}, update: function (dt) {
 
              // aumentar el tiempo que ha pasado desde el ultimo disparo
              this.tiempoEntreDisparos = this.tiempoEntreDisparos + dt;
 
              // Dispara si el tiempo ha pasado y el jugador está cerca
-             if (this.tiempoUtimoSalto > this.tiempoEntreSaltos &&
-                 Math.abs(this.body.p.x - jugadorX) < 500) {
+             if (this.tiempoUtimoDisparo > this.tiempoEntreDisparos) {
 
                 //TODO crear disparo
-                 this.tiempoUtimoSalto = 0;
+                 this.tiempoUtimoDisparo = 0;
+				 this.layer.dispaoEnemigos.push(new DisparoOctorock(this.space, this.posicion, this.layer,this.sentido));
+
              }
 
              // TODO Hacer que se mueva siguiendo algun algoritmo
-             if (this.body.getVel().x > 0) {
-                 this.sprite.flippedX = true;
-             } else {
-                 this.sprite.flippedX = false;
-             }
+
     }, getAnimacion: function (nombreAnimacion, numFrames) {
 		var framesAnimacion = [];
 		for (var i = 0; i <= numFrames; i++) {
@@ -85,16 +88,19 @@ var Octorock = cc.Class.extend({
 		return framesAnimacion;
 
 	}, moverArriba: function () {
+		this.sentido=1;
 		this.sprite.runAction(this.animMoverrriba);
 		var vMoverArriba = cc.RepeatForever.create(cc.MoveBy.create(1, cc.p(0, 50)));
 		this.sprite.runAction(vMoverArriba);
 
 	}, moverAbajo: function () {
+		this.sentido=3;
 		this.sprite.runAction(this.animMoverAbajo);
 		var vMoverAbajo = cc.RepeatForever.create(cc.MoveBy.create(1, cc.p(0, -50)));
 		this.sprite.runAction(vMoverAbajo);
 
 	}, moverDerecha:function () {
+		this.sentido=2;
 		//Se escala a 1 en la x
             this.sprite.scaleX=1;
             this.sprite.runAction(this.animCaminarDerecha);
@@ -102,7 +108,8 @@ var Octorock = cc.Class.extend({
             this.sprite.runAction(vMoverDerecha);
 
 	}, moverIquierda:function () {
-		 //Si va a la izquierda se escala a -1 para hacer flip a la animacion
+			this.sentido=4;
+		 	//Si va a la izquierda se escala a -1 para hacer flip a la animacion
             this.sprite.scaleX=-1;
             this.sprite.runAction(this.animCaminarDerecha);
             var vMoverIzquierda =cc.RepeatForever.create(cc.MoveBy.create(1, cc.p(-50,0)));
