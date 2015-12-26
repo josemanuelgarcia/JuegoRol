c7fa1c52387b12ef812bc40d28f94711c3e23af6
josemanuelgarcia <josececos@gmail.com>
2015-12-26 17:51:35 +0100

    Añadidos sprites enemigo

--- code/src/Octorok.js
+++ code/src/Octorok.js
@@ -4,6 +4,10 @@ var Octorok = cc.Class.extend({
 	animMoverAbajo: null,
 	animMoverDerecha: null,
 	animMoverIzquierda: null,
+	animDispararArriba: null,
+	animDispararAbajo: null,
+	animDispararDerecha: null,
+	animDispararIzquierda: null,
 	tiempoEntreDisparos: null,
 	tiempoUltimoDisparo: 0,
 	space: null,
@@ -11,12 +15,8 @@ var Octorok = cc.Class.extend({
 	sprite: null,
 	body: null,
 	shape: null,
-	//Sentido hacia el que esta mirando y a donde va a disparar
-	//1-> Arriba
-	//2-> Derecha
-	//3-> Abajo
-	//4-> Izquierda
-	sentido: 3,
+	//Orientacion hacia el que esta mirando y a donde va a disparar
+	orientacion: "ABAJO",
 
 	ctor: function (space, posicion, layer) {
 		this.space = space;
@@ -26,14 +26,14 @@ var Octorok = cc.Class.extend({
 		this.tiempoEntreDisparos=5+Math.floor(Math.random()*2);
 
 		this.sprite = new cc.PhysicsSprite("#Octorok_abajo0.png");
+
         // Cuerpo dinamico, SI le afectan las fuerzas
         this.body = new cp.Body(5, cp.momentForBox(1,
             this.sprite.getContentSize().width,
             this.sprite.getContentSize().height));
 
         this.body.setPos(posicion);
-        //body.w_limit = 0.02;
-        this.body.setAngle(0);
+
         this.sprite.setBody(this.body);
         // Se añade el cuerpo al espacio
         this.space.addBody(this.body);
@@ -53,9 +53,39 @@ var Octorok = cc.Class.extend({
 		this.animMoverAbajo = cc.RepeatForever.create(new cc.Animate(animacionAbajo));
 
 		//Animacion Mover Arriba
-		var framesCaminarArriba = this.getAnimacion("Octorok_arriba", 6);
+		var framesCaminarArriba = this.getAnimacion("Octorok_arriba", 2);
 		var animacionArriba = new cc.Animation(framesCaminarArriba, 0.1);
 		this.animMoverArriba = cc.RepeatForever.create(new cc.Animate(animacionArriba));
+
+		//Animacion Mover Derecha
+		var framesCaminarDerecha = this.getAnimacion("Octorok_derecha", 2);
+        var animacionDerecha = new cc.Animation(framesCaminarDerecha, 0.1);
+        this.animMoverDerecha = cc.RepeatForever.create(new cc.Animate(animacionDerecha));
+
+        //Animacion mover izquierda
+		var framesCaminarIzquierda = this.getAnimacion("Octorok_izquierda", 2);
+        var animacionIzquierda = new cc.Animation(framesCaminarIzquierda, 0.1);
+        this.animMoverIzquierda = cc.RepeatForever.create(new cc.Animate(animacionIzquierda));
+
+		//Animacion disparar hacia abajo
+		var framesDispararArriba = this.getAnimacion("Octorok_disparo_abajo", 2);
+        var animacionDisparoAbajo = new cc.Animation(framesDispararArriba, 0.1);
+        this.animDispararAbajo = cc.RepeatForever.create(new cc.Animate(animacionDisparoAbajo));
+
+        //Animacion disparar hacia arriba
+        var framesDispararArriba = this.getAnimacion("Octorok_disparo_arriba", 2);
+        var animacionDispararArriba = new cc.Animation(framesDispararArriba, 0.1);
+        this.animDispararArriba = cc.RepeatForever.create(new cc.Animate(animacionDispararArriba));
+
+        //Animacion disparar hacia la drecha
+        var framesDispararDerecha = this.getAnimacion("Octorok_disparo_derecha", 2);
+        var animacionDisparoDerecha = new cc.Animation(framesDispararDerecha, 0.1);
+        this.animDispararDerecha = cc.RepeatForever.create(new cc.Animate(animacionDisparoDerecha));
+
+        //Animacion disparar hacia la izquierda
+        var framesDispararIzquierda = this.getAnimacion("Octorok_disparo_izquierda", 2);
+        var animacionDisparoIzquierda = new cc.Animation(framesDispararIzquierda, 0.1);
+        this.animDispararIzquierda = cc.RepeatForever.create(new cc.Animate(animacionDisparoIzquierda));
 
 
 		layer.addChild(this.sprite, 10);
@@ -69,9 +99,7 @@ var Octorok = cc.Class.extend({
              // Dispara si el tiempo ha pasado
              if (this.tiempoUltimoDisparo > this.tiempoEntreDisparos) {
                  this.tiempoUltimoDisparo = 0;
-                 var disparo=new DisparoOctorok(this.space,
-                             		cc.p(this.body.p.x,this.body.p.y), this.layer, this.sentido);
-				 this.layer.disparosEnemigos.push(disparo);
+                 this.disparar();
              }
 
              // TODO Hacer que se mueva siguiendo algun algoritmo
@@ -86,19 +114,19 @@ var Octorok = cc.Class.extend({
 		return framesAnimacion;
 
 	}, moverArriba: function () {
-		this.sentido=1;
+		this.orientacion="ARRIBA";
 		this.sprite.runAction(this.animMoverrriba);
 		var vMoverArriba = cc.RepeatForever.create(cc.MoveBy.create(1, cc.p(0, 50)));
 		this.sprite.runAction(vMoverArriba);
 
 	}, moverAbajo: function () {
-		this.sentido=3;
+		this.orientacion="ABAJO";
 		this.sprite.runAction(this.animMoverAbajo);
 		var vMoverAbajo = cc.RepeatForever.create(cc.MoveBy.create(1, cc.p(0, -50)));
 		this.sprite.runAction(vMoverAbajo);
 
 	}, moverDerecha:function () {
-		this.sentido=2;
+		this.orientacion="DERECHA";
 		//Se escala a 1 en la x
             this.sprite.scaleX=1;
             this.sprite.runAction(this.animCaminarDerecha);
@@ -106,12 +134,30 @@ var Octorok = cc.Class.extend({
             this.sprite.runAction(vMoverDerecha);
 
 	}, moverIquierda:function () {
-			this.sentido=4;
-		 	//Si va a la izquierda se escala a -1 para hacer flip a la animacion
-            this.sprite.scaleX=-1;
-            this.sprite.runAction(this.animCaminarDerecha);
+			this.orientacion="IZQUIERDA";
+            this.sprite.runAction(this.animCaminarIzquierda);
             var vMoverIzquierda =cc.RepeatForever.create(cc.MoveBy.create(1, cc.p(-50,0)));
             this.sprite.runAction(vMoverIzquierda);
+
+	}, disparar: function (){
+		if(this.orientacion=="ARRIBA"){
+			this.sprite.runAction(this.animDispararArriba);
+		}
+		if(this.orientacion=="DERECHA"){
+			this.sprite.runAction(this.animDispararDerecha);
+		}
+		if(this.orientacion=="IZQUIERDA"){
+				this.sprite.scaleX=-1;
+				this.sprite.runAction(this.animDispararIzquierda);
+		}
+		if(this.orientacion=="ABAJO"){
+			this.sprite.runAction(this.animDispararAbajo);
+		}
+
+		//Crear el disparo
+		var disparo=new DisparoOctorok(this.space,
+                                     		cc.p(this.body.p.x,this.body.p.y), this.layer, this.orientacion);
+        this.layer.disparosEnemigos.push(disparo);
 	}
 
 });
