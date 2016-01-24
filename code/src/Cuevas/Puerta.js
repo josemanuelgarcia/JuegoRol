43022e8457d93b2276c707811f1898f3c682eaff
Álvaro Velázquez Vico <uo232635@uniovi.es>
2016-01-24 18:00:15 +0100

    Arreglado colision Puertas ( Infranqueables ahora joder)

--- code/src/Cuevas/Puerta.js
+++ code/src/Cuevas/Puerta.js
@@ -4,6 +4,8 @@ var Puerta = cc.Class.extend({
     space: null,
     layer: null,
     shape: null,
+    body:null,
+    shape:null,
     animacionAbrir:null,
     id:null,
     ctor: function (space, position, layer,tipo,id) {
@@ -14,11 +16,11 @@ var Puerta = cc.Class.extend({
         this.id=id;
         if(this.tipo=="normal")
         {
-        this.sprite = new cc.PhysicsSprite("#Puerta_normal.png");
+            this.sprite = new cc.PhysicsSprite("#Puerta_normal.png");
         }
         else
         {
-        this.sprite = new cc.PhysicsSprite("#Puerta_Jefe.png");
+            this.sprite = new cc.PhysicsSprite("#Puerta_Jefe.png");
         }
         var tamano = this.sprite.getContentSize();
         this.position = cc.p(position.x + tamano.width / 2, position.y + tamano.height / 2);
@@ -27,7 +29,7 @@ var Puerta = cc.Class.extend({
         this.sprite.setBody(this.body);
         this.shape = new cp.BoxShape(this.body,
             this.sprite.getContentSize().width ,
-            this.sprite.getContentSize().height );
+            this.sprite.getContentSize().height);
         this.shape.setCollisionType(tipoPuerta);
         this.space.addStaticShape(this.shape);
         this.layer.mapa.addChild(this.sprite, 2);
